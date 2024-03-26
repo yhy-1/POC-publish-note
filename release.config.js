9830e0b16b1924443e9e010df935a0db64ca14af
Y T. Hy <52711139+yhy-1@users.noreply.github.com>
2024-03-26 16:06:52 -0400

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -7,29 +7,7 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/git",
-      {
-        // eslint-disable-next-line no-template-curly-in-string
-        message: "chore(release): ${nextRelease.version} [skip ci]",
-        assets: [
-          "build/*",
-          "CHANGELOG.md",
-          "package.json",
-          "package-lock.json",
-        ],
-      },
-    ],
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "build/*",
-            label: "Build",
-          },
-        ],
-      },
-    ],
+    "@semantic-release/git",
+    "@semantic-release/github",
   ],
 };
